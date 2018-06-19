8ed9a2e543abb84f46d93f5e21efd7436df1f1d9
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-06-19 11:57:39 +0100

    Script added

---
+++ Rscript/merge_sims.R
@@ -0,0 +1,16 @@
+# merge simulation output files
+
+args <- commandArgs(TRUE)
+n <- length(args)
+input.files <- args[1:(n-1)]
+output.file <- args[n]
+
+P <- lapply(input.file, function(file) {
+  if(file.exists(file)) {
+    return(read.delim(file, sep="\t", header=TRUE))
+  }
+})
+P <- do.call(rbind, P)
+rownames(P) <- NULL
+
+saveRDS(P, file=output.file)
