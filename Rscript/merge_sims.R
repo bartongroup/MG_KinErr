bb2bb54334efef51174fc578a79e9d6bf3cbfac8
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-06-19 12:02:55 +0100

    Bug fix

--- Rscript/merge_sims.R
+++ Rscript/merge_sims.R
@@ -5,7 +5,7 @@ n <- length(args)
 input.files <- args[1:(n-1)]
 output.file <- args[n]
 
-P <- lapply(input.file, function(file) {
+P <- lapply(input.files, function(file) {
   if(file.exists(file)) {
     return(read.delim(file, sep="\t", header=TRUE))
   }
